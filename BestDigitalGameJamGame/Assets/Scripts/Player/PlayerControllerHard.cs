962a6992888705b4c58d7ffc33ce8ce9e16d4fa8
CharliSIO <c.jones89623@gmail.com>
2025-01-23 20:55:30 +1300

    Player can stand up
    Player can stand up and pivots around the base (not centre origin)

--- BestDigitalGameJamGame/Assets/Scripts/Player/PlayerControllerHard.cs
+++ BestDigitalGameJamGame/Assets/Scripts/Player/PlayerControllerHard.cs
@@ -7,9 +7,11 @@ public class PlayerControllerHard : MonoBehaviour
 {
     public float fMoveSpeed;
     public float fJumpForce;
+    public float fGravityForce;
+
     private float fStandUpSpeed = 1.0f;
     private float fStandUpPos = 0.0f;
-    public GameObject PlayerBottomOrigin;
+    public GameObject PlayerObject;
 
     private Rigidbody PlayerBody;
     public Camera PlayerCamera;
@@ -21,6 +23,9 @@ public class PlayerControllerHard : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         PlayerBody = gameObject.GetComponentInChildren<Rigidbody>();
         PlayerCollider = gameObject.GetComponentInChildren<CapsuleCollider>(); //If other capsule colliders are added to the player this will break
+
+        // Adjust the position of PlayerBottomOrigin to be at the bottom of PlayerBody capsule
+        //PlayerBottomOrigin.transform.position = PlayerBody.transform.position - (PlayerBody.transform.up * PlayerBody.GetComponent<Collider>().bounds.extents.y);
     }
     
     void Update()
@@ -28,34 +33,51 @@ public class PlayerControllerHard : MonoBehaviour
         Vector3 MoveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         float fRealMoveSpeed = Input.GetKey(KeyCode.LeftShift) ? fMoveSpeed * 2 : fMoveSpeed; // Double move speed when 'sprinting'
         
-        if (Input.GetKeyDown(KeyCode.Space) &&Physics.Raycast(PlayerCollider.ClosestPoint(new Vector3(transform.position.x,0,transform.position.z)), Vector3.down, 0.5f, LayerMask.NameToLayer("Respawn"))) //TODO Fix collision layers
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(PlayerCollider.ClosestPoint(new Vector3(transform.position.x,0,transform.position.z)), Vector3.down, 0.5f, LayerMask.NameToLayer("Respawn"))) //TODO Fix collision layers
         {
-            PlayerBody.AddForce(new Vector3(0,fJumpForce,0), ForceMode.Impulse);
+            PlayerBody.AddForce(new Vector3(0, fJumpForce,0), ForceMode.Impulse);
         }
         
-        // TO GET WORKING
-        if (Input.GetKey(KeyCode.LeftControl)) // not yet working
+        // PLAYER CAN STAND UP
+        if (Input.GetKey(KeyCode.LeftControl) && fStandUpPos < 1.0f) // not yet working
         {
-            // stand up speed in seconds
-            // add to stand up pos for lerp
+            //// stand up speed in seconds
+            //// add to stand up pos for lerp
+            //// if it exceeds 1, clamp to 1
             fStandUpPos += fStandUpSpeed * Time.deltaTime;
-            Debug.Log(fStandUpPos);
 
-            //Vector3 newRotationDirection = Vector3.RotateTowards(Vector3.Normalize(transform.TransformDirection(0, 10, 0)), Vector3.Normalize(Vector3.up), fRotationPerFrame, 0.0f);
-            Vector3 newRotationDirection = Vector3.Lerp(Vector3.Normalize(PlayerBody.transform.up), Vector3.Normalize(Vector3.up), fStandUpPos);
+            // pivot point at base of player capsule
+            Vector3 v3PivotPoint = (PlayerBody.transform.position - (PlayerBody.transform.up * PlayerBody.GetComponent<Collider>().bounds.extents.y));
 
-            // Draw a ray pointing at target direction
-            Debug.DrawRay(PlayerBottomOrigin.transform.position, newRotationDirection, Color.red, 0.0f);
-            //Debug.DrawRay(PlayerBottomOrigin.transform.position, PlayerBody.transform.up, Color.green, 0.0f);
-            //Debug.DrawRay(PlayerBottomOrigin.transform.position, Vector3.up, Color.blue, 0.0f);
+            // new up vector direction
+            Vector3 newDirection;
 
-            PlayerBottomOrigin.transform.rotation = Quaternion.FromToRotation(PlayerBody.transform.up, newRotationDirection);
-            //PlayerBottomOrigin.transform.rotation = Quaternion.LookRotation(PlayerBody.transform.up, newRotationDirection);
+            if (fStandUpPos >= 1.0f)
+            {
+                newDirection = Vector3.up;
+            }
+            else
+            {
+                // calculate the rotated vector from old up position to world up, if rotating at the stand up speed, as per each frame
+                newDirection = Vector3.RotateTowards(PlayerBody.transform.up, Vector3.up, fStandUpSpeed * Time.deltaTime, 0.0f);
+            }
+
+            // rotate around the pivot by the axis that is formed by the cross product of old and new direction
+            PlayerBody.transform.RotateAround(v3PivotPoint, Vector3.Cross(PlayerBody.transform.up, newDirection), Vector3.Angle(PlayerBody.transform.up, newDirection));
+        }
+        else
+        {
+            // if user has released control key, allow them to stand up again
+            if (!Input.GetKey(KeyCode.LeftControl))
+            {
+                fStandUpPos = 0.0f; 
+            }
         }
 
         Vector3 MoveVelocity = PlayerCamera.transform.TransformDirection(MoveDir) * (fRealMoveSpeed * Time.deltaTime);
         PlayerBody.velocity = new Vector3(MoveVelocity.x, PlayerBody.velocity.y, MoveVelocity.z);
         
         transform.position = PlayerBody.position;
+        transform.rotation = PlayerBody.rotation;
     }
 }
