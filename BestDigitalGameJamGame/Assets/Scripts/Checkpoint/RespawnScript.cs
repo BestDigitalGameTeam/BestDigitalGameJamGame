98172410a4e056444cf4d8b256b156eb6763457e
Zanymacman <zanymacman@gmail.com>
2025-01-25 11:30:40 +1300

    Level Beginnings
    Also added fast restart with 'r' press

--- BestDigitalGameJamGame/Assets/Scripts/Checkpoint/RespawnScript.cs
+++ BestDigitalGameJamGame/Assets/Scripts/Checkpoint/RespawnScript.cs
@@ -6,18 +6,31 @@ public class RespawnScript : MonoBehaviour
 {
     public GameObject Player;
     public GameObject Checkpoint;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Respawn();
+        }
+    }
     
     private void OnTriggerEnter(Collider other)//if player touches death plane, respawn them at current checkpoint
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            //move player
+            Respawn();
+        }
+    }
+
+    private void Respawn()//respawn function
+    {
+            //move player to current checkpoint
             Player.transform.position = Checkpoint.transform.GetChild(0).gameObject.transform.position;
             //rotate player
             Player.transform.eulerAngles = new Vector3(0,0,0);
             //stop momentum
             Player.GetComponentInChildren<Rigidbody>().velocity = Vector3.zero;
             Player.GetComponentInChildren<Rigidbody>().angularVelocity = Vector3.zero;
-        }
     }
 }
