1e12a4eb02e51050f3f1be22228f551bbd52104a
Zanymacman <zanymacman@gmail.com>
2025-01-25 14:07:15 +1300

    Fixed Checkpoints

---
+++ BestDigitalGameJamGame/Assets/Scripts/RefManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefManager : MonoBehaviour
+{
+    public GameObject PlayerRef;
+    public GameObject DeathPlane;
+    public GameObject Ref3;
+}
